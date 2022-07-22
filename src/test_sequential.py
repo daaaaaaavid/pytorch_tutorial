7e96b77ec9ae7dbe29b9f24855f3d2cff23493c2
daaaaaaavid <b0932296807@gmail.com>
2022-07-22 08:45:53 +0800

    commit

---
+++ src/test_sequential.py
@@ -0,0 +1,30 @@
+import torch
+from torch import nn
+from torch.nn import Conv2d, MaxPool2d, Flatten, Linear, Sequential
+from torch.utils.tensorboard import SummaryWriter
+
+
+class module(nn.Module):
+    def __init__(self):
+        super(module,self).__init__()
+        self.model1 = Sequential(
+            Conv2d(3,32,5,padding=2),
+            MaxPool2d(2),
+            Conv2d(in_channels=32, out_channels=32, kernel_size=5, padding=2, stride=1),
+            MaxPool2d(2),
+            Conv2d(in_channels=32, out_channels=64, kernel_size=5, padding=2),
+            MaxPool2d(2),
+            Flatten(),
+            Linear(1024, 64),
+            Linear(64, 10),
+        )
+    def forward(self,x):
+        output = self.model1(x)
+        return output
+
+M = module()
+input = torch.ones((64,3,32,32))
+print(M(input).shape)
+writer = SummaryWriter("../logs")
+writer.add_graph(M,input)
+writer.close()
